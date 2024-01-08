d8c0a2312226e515097f413e415d01fc4732f3cd
Wonbin Lee <wblee800@gmail.com>
2024-01-08 15:02:30 +0900

    Edit a WBFMOD

--- wblee_engine_source/WBFMOD.cpp
+++ wblee_engine_source/WBFMOD.cpp
@@ -33,10 +33,10 @@ namespace wb
 	void WBFMOD::Set3DListenerAttributes(const math::Vector2* pos)
 	{
 		//-1 ~ 1
-		FMOD_VECTOR fmodPos(0.0f, 0.0f, 0.3f);
-		FMOD_VECTOR fmodVel(0.0f, 0.0f, 0.0f);
-		FMOD_VECTOR fmodForward(0.0f, 0.0f, 1.0f);
-		FMOD_VECTOR fmodUp(0.0f, 1.0f, 0.0f);
+		FMOD_VECTOR fmodPos = { 0.0f, 0.0f, 0.0f };
+		FMOD_VECTOR fmodVel = { 0.0f, 0.0f, 0.0f };
+		FMOD_VECTOR fmodForward = { 0.0f, 0.0f, 0.0f };
+		FMOD_VECTOR fmodUp = { 0.0f, 0.0f, 0.0f };
 
 		mCoreSystem->set3DListenerAttributes(0, &fmodPos, &fmodVel, &fmodForward, &fmodUp);
 	}
